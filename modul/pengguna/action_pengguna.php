75062a3d4e1562900469f027ead9e805d8a698a3
jokopurnomoa <jokopurnomoa@gmail.com>
2013-12-03 16:33:14 +0700

    Update tambah pengguna

---
+++ modul/pengguna/action_pengguna.php
@@ -0,0 +1,52 @@
+<?php
+include("../../lib_php/connection.php");
+/**
+ * Created by JetBrains PhpStorm.
+ * User: JOKO PURNOMO A
+ * Date: 12/3/13
+ * Time: 4:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+$action = "";
+$id_user = "";
+$nama_user = "";
+$password = "";
+$hak_akses = "";
+
+if(isset($_GET['action'])){
+    $action = $_GET['action'];
+}
+
+if(isset($_GET['id_user'])){
+    $id_user = $_GET['id_user'];
+}
+
+if(isset($_GET['nama_user'])){
+    $nama_user = $_GET['nama_user'];
+}
+
+if(isset($_GET['password'])){
+    $password = $_GET['password'];
+}
+
+if(isset($_GET['hak_akses'])){
+    $hak_akses = $_GET['hak_akses'];
+}
+
+function tambah_user($id_user, $nama_user, $password, $hak_akses){
+    if($id_user == "" || $nama_user == "" || $password == "" || $hak_akses == ""){
+        header("location:../../index.php?modul=pengguna&submodul=tambah_pengguna&result=failed_h");
+        return;
+    }
+    $insert = mysql_query("INSERT INTO user VALUES('$id_user','$nama_user','$hak_akses','$password')");
+    if($insert){
+        header("location:../../index.php?modul=pengguna&submodul=tambah_pengguna&result=success_h");
+    } else {
+        header("location:../../index.php?modul=pengguna&submodul=tambah_pengguna&result=failed_h");
+    }
+}
+
+if($action == "tambah_user"){
+    tambah_user($id_user,$nama_user,$password,$hak_akses);
+}
+?>
--- modul/pengguna/tambah_pengguna.php
+++ modul/pengguna/tambah_pengguna.php
@@ -14,6 +14,69 @@
 <div class="content">
     <div class="row-fluid">
         <!-- Code Here -->
+        <div class="span8">
+            <?php
+            $result = "";
+            if(isset($_GET['result'])){
+                $result = $_GET['result'];
+            }
 
+            if($result == 'success_h'){
+                ?>
+                <div class="alert alert-success">
+                    <strong>Berhasil tambah pengguna.</strong>
+                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                </div>
+            <?php
+            } else if($result == 'failed_h'){
+                ?>
+                <div class="alert alert-error">
+                    <strong>Gagal tambah pengguna!</strong>
+                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                </div>
+            <?php
+            }
+            ?>
+
+            <form action="modul/pengguna/action_pengguna.php" method="get">
+                <div class="block">
+                    <div class="head">
+                        <h2>Form Tambah Pengguna</h2>
+                    </div>
+                    <div class="content np">
+                        <input type="hidden" name="action" value="tambah_user">
+                        <div class="controls-row">
+                            <div class="span3">ID Pengguna</div>
+                            <div class="span9"><input type="text" name="id_user" placeholder="ID Pengguna"/></div>
+                        </div>
+                        <div class="controls-row">
+                            <div class="span3">Nama Pengguna</div>
+                            <div class="span9">
+                                <input type="text" name="nama_user" placeholder="Nama Pengguna"/>
+                            </div>
+                        </div>
+                        <div class="controls-row">
+                            <div class="span3">Password</div>
+                            <div class="span9">
+                                <input type="password" name="password" placeholder="Password"/>
+                            </div>
+                        </div>
+                        <div class="controls-row">
+                            <div class="span3">Hak Akses</div>
+                            <div class="span9">
+                                <label class="radio-uni inline"><input type="radio" name="hak_akses" value="super" class="uni" checked="checked"/> Super Admin</label>
+                                <label class="radio-uni inline"><input type="radio" name="hak_akses" value="admin" class="uni"/> Admin</label>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="footer">
+                        <div class="side fr">
+                            <input type="submit" class="btn btn-primary" value="Simpan">
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
